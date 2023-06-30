9ab6d4a206f15c923e9bc3f202752d5b91a1a1ff
GBann <GTBannon@protonmail.com>
2023-06-30 08:56:59 -0600

    Created 404, updt imgs created css files

--- scripts.js
+++ scripts.js
@@ -2,3 +2,25 @@
 function notifyClick(){
     alert("You just clicked something");
 }
+
+function clearContact() {
+    document.getElementById("inputName").value = '';
+    document.getElementById("inputEmail").value = '';
+    document.getElementById("messageText").value = '';
+    
+    
+}
+
+function submitForm() {
+    document.getElementById("inputName").value = '';
+    document.getElementById("inputEmail").value = '';
+    document.getElementById("messageText").value = '';
+    alert("Form Successfully Submitted");
+}
+function over(element){
+    // element.muted = true;
+    element.play();
+}
+function out(element){
+    element.pause();
+}
